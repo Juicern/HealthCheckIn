91eabb0ada99b36cd0f546fbf087c51d8c10e306
RickyChu <qs4869@outlook.com>
2021-01-11 20:44:52 +0800

    完善loginhelper类对数据库helper类的调用

--- LoginHelper.cs
+++ LoginHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,35 +13,26 @@ namespace HealthCheckIn
         {
             var dictLoginInfo = new Dictionary<string, string>();
             dictLoginInfo.Add("account", ConfigHelper.GetAppConfig("account"));
-            dictLoginInfo.Add("password", ConfigHelper.GetAppConfig("password"));
+            dictLoginInfo.Add("password", ToMD5(ConfigHelper.GetAppConfig("password")));
             return dictLoginInfo;
         }
         public static void UpdateLoginInfoToConfig(Dictionary<string, string> dictLoginInfo)
         {
             ConfigHelper.UpdateAppConfig("account", dictLoginInfo["account"]);
-            ConfigHelper.UpdateAppConfig("password", dictLoginInfo["password"]);
+            ConfigHelper.UpdateAppConfig("password", ToMD5(dictLoginInfo["password"]));
         }
-        public static bool CheckAccount(string strAccount)
+
+        //check if this account is in db
+        public static bool CheckAccount(string strAccount) => new DataBaseHelper().CheckAccount(strAccount);
+        //check if this login is correct
+        public static bool CheckLogin(string strAccount, string strPassword) => new DataBaseHelper().CheckLogin(strAccount, ToMD5(strPassword));
+        public static (bool, string) Register(string strAccount, string strPassword) => new DataBaseHelper().Register(strAccount, ToMD5(strPassword));
+        private static string ToMD5(string strs)
         {
-            //check if this account is in db
-            return true;
-        }
-        public static bool CheckLogin(string strAccount, string strPassword)
-        {
-            //check if this login is correct
-            return true;
-        }
-        public static (bool, string) Register(string strAccount, string strPassword)
-        {
-            try
-            {
-                //register
-                return (true, "ok");
-            }
-            catch
-            {
-                return (false, "未知原因");
-            }
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] bytes = Encoding.Default.GetBytes(strs);
+            byte[] encryptdata = md5.ComputeHash(bytes);
+            return Convert.ToBase64String(encryptdata);
         }
     }
 }
