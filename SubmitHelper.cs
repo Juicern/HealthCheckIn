12b429e340e9778df7438a777f7119127739ac1f
RickyChu <qs4869@outlook.com>
2021-01-11 18:22:06 +0800

    完善提交窗口的helper类

---
+++ SubmitHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCheckIn
+{
+    public static class SubmitHelper
+    {
+        public static Dictionary<string, string> GetSubmitInfoFromConfig()
+        {
+            var dictSubmitInfo = new Dictionary<string, string>();
+            dictSubmitInfo.Add("temperature", ConfigHelper.GetAppConfig("temperature"));
+            dictSubmitInfo.Add("health", ConfigHelper.GetAppConfig("health"));
+            dictSubmitInfo.Add("location", ConfigHelper.GetAppConfig("location"));
+            return dictSubmitInfo;
+        }
+        public static void UpdateSubmitInfoToConfig(Dictionary<string, string> dictSubmitInfo)
+        {
+            ConfigHelper.UpdateAppConfig("temperature", dictSubmitInfo["temperature"]);
+            ConfigHelper.UpdateAppConfig("health", dictSubmitInfo["health"]);
+            ConfigHelper.UpdateAppConfig("location", dictSubmitInfo["location"]);
+        }
+    }
+}
