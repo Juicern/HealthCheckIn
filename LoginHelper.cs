45f3139940040fe0c8bbf5d349112b6f901fd50d
RickyChu <qs4869@outlook.com>
2021-01-11 18:22:26 +0800

    将登录读取配置文件的操作放入helper类

--- LoginHelper.cs
+++ LoginHelper.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace HealthCheckIn
 {
-    public class LoginHelper
+    public static class LoginHelper
     {
+        public static Dictionary<string, string> GetLoginInfoFormConfig()
+        {
+            var dictLoginInfo = new Dictionary<string, string>();
+            dictLoginInfo.Add("account", ConfigHelper.GetAppConfig("account"));
+            dictLoginInfo.Add("password", ConfigHelper.GetAppConfig("password"));
+            return dictLoginInfo;
+        }
+        public static void UpdateLoginInfoToConfig(Dictionary<string, string> dictLoginInfo)
+        {
+            ConfigHelper.UpdateAppConfig("account", dictLoginInfo["account"]);
+            ConfigHelper.UpdateAppConfig("password", dictLoginInfo["password"]);
+        }
         public static bool CheckAccount(string strAccount)
         {
             //check if this account is in db
