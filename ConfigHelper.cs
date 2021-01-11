be47aa78c5b46bef60375a6c55ee45faac50c2c3
RickyChu <qs4869@outlook.com>
2021-01-11 16:33:40 +0800

    完成对配置文件修改和读取的helper类

---
+++ ConfigHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace HealthCheckIn
+{
+    public class ConfigHelper
+    {
+        public static string GetAppConfig(string strKey)
+        {
+            string file = System.Windows.Forms.Application.ExecutablePath;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(file);
+            foreach (string key in config.AppSettings.Settings.AllKeys)
+            {
+                if (key == strKey)
+                {
+                    return config.AppSettings.Settings[strKey].Value.ToString();
+                }
+            }
+            return string.Empty;
+        }
+        public static void UpdateAppConfig(string newKey, string newValue)
+        {
+            string file = System.Windows.Forms.Application.ExecutablePath;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(file);
+            if (config.AppSettings.Settings.AllKeys.Any(x => x == newKey))
+            {
+                config.AppSettings.Settings.Remove(newKey);
+            }
+            config.AppSettings.Settings.Add(newKey, newValue);
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
